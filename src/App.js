ccf9ba4352cf114ca4f0b8edb6c2b4f4dabdd08a
You Song Hou <yousonghou@gmail.com>
2022-03-06 00:30:09 -0500

    refactor: install 'react-p5' and move the simple Scratch-Off reveal content effects to react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Sketch from "react-p5";
 import './App.css';
 
 function App() {
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.strokeWeight(20);
+    if(p5.mouseIsPressed === true) {
+      p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+    }
+  };
+  
   return (
     <div className="App">
-      <h1>DeScratchOff Card</h1>
+      <section>
+        <div class="box">
+            <h2>DeScratchOff Card</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ducimus voluptatibus numquam, aliquid vitae, praesentium provident iste vero laborum amet molestiae voluptatum nam harum, tempora saepe odit rerum voluptatem enim! Dicta quis dolor voluptatibus vitae, consectetur sint laborum aspernatur neque asperiores tenetur similique veniam iste tempore quas soluta ratione maiores quam quae officiis? Exercitationem culpa inventore possimus necessitatibus, est vitae nesciunt doloremque ipsum ut atque amet, voluptatibus excepturi quae reprehenderit quidem sit nam optio? Aut dolores, facere exercitationem repudiandae consectetur hic similique, quibusdam velit perferendis nemo suscipit deleniti illum quisquam maiores, voluptates libero eum! Ex nam laborum sint harum nesciunt?</p>
+        </div>
+      </section>
+      <Sketch setup={setup} draw={draw} />
     </div>
   );
 }
