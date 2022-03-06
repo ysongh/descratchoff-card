f1ceb3cf4a60807cd79d94204cf17e9ca037c62f
You Song Hou <yousonghou@gmail.com>
2022-03-06 02:05:31 -0500

    refactor: add a component for Scratch off Card

---
+++ src/components/Scratchoff.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Sketch from "react-p5";
+
+function Scratchoff() {
+	const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.strokeWeight(20);
+    if(p5.mouseIsPressed === true) {
+      p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+    }
+  };
+
+	return (
+		<div>
+			<section>
+        <div class="box">
+            <h2>DeScratchOff Card</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </section>
+      <Sketch setup={setup} draw={draw} />
+		</div>
+	)
+}
+
+export default Scratchoff;
