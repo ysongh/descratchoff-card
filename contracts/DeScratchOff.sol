ee938eeac595c6a592e2288d08d65903529d767f
You Song Hou <yousonghou@gmail.com>
2022-03-09 23:44:42 -0500

    feat: import Openzeppelin ERC721 contract

--- contracts/DeScratchOff.sol
+++ contracts/DeScratchOff.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DeScratchOff is VRFConsumerBase{
+contract DeScratchOff is VRFConsumerBase, ERC721{
     bytes32 internal keyHash;
     uint256 internal fee;
+
+    using Counters for Counters.Counter;
+    Counters.Counter public _tokenIds;
     
     uint256 public randomResult;
     uint public scratchCardSupply = 0;
@@ -38,16 +43,21 @@ contract DeScratchOff is VRFConsumerBase{
             0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
             0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
         )
+        ERC721("De-Scratch Card", "DSC") 
     {
         keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
         fee = 0.0001 * 10 ** 18; // 0.0001 LINK
     }
 
     /** 
-     * Buy a Scratch Card
+     * Buy a Scratch Card NFT
      */
     function buyScratchCard() external {
         scratchCardSupply++;
+
+        _tokenIds.increment();
+        uint _tokenId = _tokenIds.current();
+        _safeMint(msg.sender, _tokenId);
 
         scratchCards[scratchCardSupply] = ScratchCard(scratchCardSupply, new uint[](0), false, msg.sender);
         emit ScratchCardPurchase(scratchCardSupply, new uint[](0), false, msg.sender);
@@ -70,7 +80,7 @@ contract DeScratchOff is VRFConsumerBase{
         require(_scratchCard.isScratch == false, "You already use this Scratch Card");
         _scratchCard.isScratch = true;
 
-        //getRandomNumber();
+        getRandomNumber();
 
         for(uint i = 0; i < 9; i++){
             uint256 _randomNumber = uint256(keccak256(abi.encode(randomResult, i))) % 9 + 1;
