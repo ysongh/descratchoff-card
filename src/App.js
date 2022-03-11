1190d938b24fbc7e65957a4316972cd127f3d919
You Song Hou <yousonghou@gmail.com>
2022-03-10 20:22:49 -0500

    feat: connect the client to the contract with ethers.js and web3modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import ScratchCard from './pages/ScratchCard' ;
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [DSOContract, setDSOContract] = useState(null);
+
   return (
     <div>
-      <Navbar />
+      <Navbar
+        walletAddress={walletAddress}
+        setWalletAddress={setWalletAddress}
+        setDSOContract={setDSOContract} />
       <ScratchCard />
     </div>
   );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
 
-function Navbar() {
+import DeScratchOff from '../../artifacts/contracts/DeScratchOff.sol/DeScratchOff.json';
+
+const DESCRATCHOFF_ADDRESS = "0xfDf46F470b85F5ab708e2cFb2Ca48f1bF1f54a8B";
+
+function Navbar({ walletAddress, setWalletAddress, setDSOContract }) {
+  const connectWallet = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);  
+    console.log(provider);
+
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setWalletAddress(address);
+
+    let contract = new ethers.Contract(DESCRATCHOFF_ADDRESS, DeScratchOff.abi, signer);
+    setDSOContract(contract);
+  }
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="/">DeScratchOff Card</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">DeScratchOff Card</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/">Home</a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">Home</a>
             </li>
           </ul>
-          <button class="btn btn-outline-success" type="submit">Connect to Wallet</button>
+          <button className="btn btn-outline-success" type="submit"  onClick={connectWallet}>
+            {walletAddress ? walletAddress.substring(0,8) + "..." + walletAddress.substring(34,42) : "Connect to Wallet"}
+          </button>
         </div>
       </div>
     </nav>
