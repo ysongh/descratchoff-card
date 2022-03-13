3907aa7dfca7bff211b66f33306875b1189aafdf
You Song Hou <yousonghou@gmail.com>
2022-03-13 11:48:45 -0400

    feat: add a route to get NFT token IDs for contract using Covalent API

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
 const app = express();
 
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
 app.get('/', (req, res) => res.send('Server Work'));
+
+app.get('/nfts', async (req, res) => {
+    const nfts = await fetch(`https://api.covalenthq.com/v1/80001/tokens/${process.env.DESCRATCHOFF_ADDRESS}/nft_token_ids/?quote-currency=USD&format=JSON&key=${process.env.COVALENT_APIKEY}`);
+    const { data } = await nfts.json();
+
+    res.status(201).json({ data });
+});
 
 const port = process.env.PORT || 4000;
 
