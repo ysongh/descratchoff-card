3ad5dd07fc1d8d85d0f90a6ad641c021a5f13da2
You Song Hou <yousonghou@gmail.com>
2022-04-25 00:36:39 -0400

    feat: add a link to Polygon scan

--- src/components/ArtistCollectionsList.js
+++ src/components/ArtistCollectionsList.js
@@ -10,7 +10,9 @@ function ArtistCollectionsList({ artistCards, purchaseCard, purchaseLoading }) {
             <div className="card-body">
               <h5 className="card-title">Card #{card.id.toString()}</h5>
               <p className="card-text">
-                {card.artist.substring(0,5) + "..." + card.artist.substring(37,42)}
+                <a href={`https://mumbai.polygonscan.com/address/${card.artist}`} target="_blank" rel="noopener noreferrer">
+                  {card.artist.substring(0,8) + "..." + card.artist.substring(34,42)}
+                </a>
               </p>
               {purchaseLoading
                 ? <p>Loading...</p>
