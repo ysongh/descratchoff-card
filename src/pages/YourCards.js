c69583836a3bae2917bd66f031f1a7f2075c6043
You Song Hou <yousonghou@gmail.com>
2022-04-26 00:34:31 -0400

    feat: add a loading indicator

--- src/pages/YourCards.js
+++ src/pages/YourCards.js
@@ -5,37 +5,47 @@ import ScratchCardsList from '../components/ScratchCardsList';
 
 function YourCards({ walletAddress, DSOContract }) {
   const [scratchCards, setScratchCards] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if(DSOContract) getScratchCard();
   }, [DSOContract])
 
   const getScratchCard = async () => {
-    // const nfts = await fetch(`https://api.covalenthq.com/v1/80001/tokens/${DESCRATCHOFF_ADDRESS}/nft_token_ids/?quote-currency=USD&format=JSON&key=${COVALENT_APIKEY}`);
-    // const { data } = await nfts.json();
-    
-    const totalCards = await DSOContract.scratchCardSupply();
-    console.log(+totalCards.toString());
-
-    let cards = [];
-    for(let i = 1; i <= +totalCards.toString(); i++){
-      const nft = await DSOContract.scratchCards(i);
+    try {
+      setLoading(true);
+      // const nfts = await fetch(`https://api.covalenthq.com/v1/80001/tokens/${DESCRATCHOFF_ADDRESS}/nft_token_ids/?quote-currency=USD&format=JSON&key=${COVALENT_APIKEY}`);
+      // const { data } = await nfts.json();
       
-      if(nft.owner == walletAddress){
-        cards.push(nft);
-      }
-    }
+      const totalCards = await DSOContract.scratchCardSupply();
+      console.log(+totalCards.toString());
 
-    console.log(cards);
-    setScratchCards(cards);
+      let cards = [];
+      for(let i = 1; i <= +totalCards.toString(); i++){
+        const nft = await DSOContract.scratchCards(i);
+        
+        if(nft.owner == walletAddress){
+          cards.push(nft);
+        }
+      }
+
+      console.log(cards);
+      setScratchCards(cards);
+      setLoading(false);
+    } catch(error) {
+      console.error(error);
+      setLoading(false);
+    }
   }
 
   return (
     <div className='container'>
       <h2 className='mt-3 mb-3'>Your Digital scratch cards</h2>
-      {scratchCards.length
-        ? <ScratchCardsList scratchCards={scratchCards} />
-        : <p className="text-danger">You do not have any digital scratch cards </p>
+      {loading
+        ? <p>Loading...</p>
+        : scratchCards.length
+          ? <ScratchCardsList scratchCards={scratchCards} />
+          : <p className="text-danger">You do not have any digital scratch cards </p>
       }
     </div>
   )
