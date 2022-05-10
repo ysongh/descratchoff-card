a2d49cce5593a1e1caf21c8ae6fe934d68ab4467
You Song Hou <yousonghou@gmail.com>
2022-05-10 00:35:49 -0400

    refactor: change the background color of the Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
   return(
-    <footer className="bg-success p-3 text-center text-white mt-5 fixed-bottom">
+    <footer className="footer-bg p-3 text-center text-dark mt-5 fixed-bottom ">
       &copy;{new Date().getFullYear()} DeScratchOff Card
     </footer>
   );
