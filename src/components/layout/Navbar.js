63ed96d431b6f2cd9f5679c27611d2970aa68a95
You Song Hou <yousonghou@gmail.com>
2022-06-22 21:50:07 -0400

    refactor: move toggler on the left and logo on the right for Navbar on the phone screen

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -81,12 +81,12 @@ function Navbar({ walletAddress, domainData, setDomainData, maticBalance, setmat
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">
-          <img src="./logo.png" alt="logo" style={{ width: '80px' }} />
-        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        <Link className="navbar-brand" to="/">
+          <img src="./logo.png" alt="logo" style={{ width: '80px' }} />
+        </Link>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
